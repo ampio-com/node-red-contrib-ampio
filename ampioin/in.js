eca3ce4a9eeaccb296a65e002baf80538c935738
Krzysztof Wesolowski <krzysztof.wesolowski@rainlabs.pl>
2019-10-21 01:24:56 +0200

    Extract common mac/ioid/mqqt client handling
    Extract utilities that are needed for multiple node types
    to be simple / easy to create and maintain.

--- ampioin/in.js
+++ ampioin/in.js
@@ -1,49 +1,25 @@
 module.exports = function(RED) {
     function ampioin(config) {
-        
-        RED.nodes.createNode(this,config);
-        var context = this.context();
-        var node = this;
-        this.mac = config.mac;
-        this.ioid = config.ioid;
-        this.valtype = config.valtype;
-        this.retainignore = config.retainignore;
-        this.srvaddress = config.srvaddress;
-        var mqtt = require('mqtt');
-        var client  = mqtt.connect('mqtt://'+node.srvaddress);
-        var n=false;
-        var m=false;
+        RED.nodes.createNode(this, config);
+        const node = this;
 
-        var mac = node.mac;
-        var ioid = node.ioid;
-        var valtype = node.valtype;
-        var retainignore = node.retainignore
+        au = require('../generic/ampio-utils')
+        const mac = au.sanitize_mac(config.mac);
+
+        //mutable as valtype can overide
+        let ioid = au.sanitize_ioid(config.ioid);
+        let valtype = config.valtype;
+
+        const retainignore = config.retainignore;
+
+        var client  = au.setup_mqtt_client(node, config)
+        au.setup_node_status_from_mqtt_client(node, client)
+
         var JustConnected;
-        var RisingEdgeDetection=false;
+        var RisingEdgeDetection = false;
         if (valtype == "re"){
             valtype = "i";
             RisingEdgeDetection = true;
-        }
-
-        mac = mac.toUpperCase();
-        node.status({fill:"yellow",shape:"dot",text:"not connected"});
-
-        while(m==false){
-            if(mac[0]=='0'){
-                mac = mac.substring(1);
-            }
-            else{
-                m=true;
-            }
-        }
-
-        while(n==false){
-            if(ioid[0]=='0'){
-                ioid = ioid.substring(1);
-            }
-            else{
-                n=true;
-            }
         }
 
         client.on('connect', function () {
@@ -96,35 +72,16 @@ module.exports = function(RED) {
             }
             client.subscribe(SubPath, function (err) { //topic to subscribe
                 if (!err && mac!="" && ioid!="" && valtype!="") {
-                    node.status({fill:"green",shape:"dot",text:"connected"});
+                    node.status({fill: "green", shape: "dot", text: "connected"});
                     JustConnected=true;
-                }
-                else{
-                    node.status({fill:"red",shape:"ring",text:"fill properties"});
+                } else {
+                    node.status({fill: "red", shape: "ring", text:"fill properties"});
                 }
             })
             
         })
 
-        client.on('error', function () {
-            node.status({fill:"red",shape:"dot",text:"unable to connect"});
-        })
 
-        client.on('reconnect', function () {
-            node.status({fill:"yellow",shape:"dot",text:"reconnecting"});
-        })
-
-        client.on('error', function() {
-            node.status({fill:"red",shape:"dot",text:"error"});
-        })
-
-        client.on('close', function() {
-                node.status({fill:"red",shape:"dot",text:"connection closed"});
-        })
-
-        client.on('offline', function() {
-            node.status({fill:"red",shape:"dot",text:"disconnected"});
-        })
 
         client.on('message', function (topic, message) {
 
